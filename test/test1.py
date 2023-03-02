e72f9281d71ee109134f2cd604da197613de6406
yeep <82635195@qq.com>
2023-03-02 10:56:09 +0800

    更新logsofmax、softmax+log

--- test/test1.py
+++ test/test1.py
@@ -1,11 +1,9 @@
 import torch
+from torch.nn import CrossEntropyLoss
 from torch.nn import functional as F
-
-x = F.log_softmax(torch.randn(size=(16, 10)), dim=1)
-y = torch.randint(0, 9, (16, ))
-loss1 = F.nll_loss(x, target=y)
-
-x1 = F.softmax(x,dim=1)
-o1 = x1[range(16),y]
-loss2 = torch.mean(-torch.log(o1))
-print(loss1 == loss2)
+from torchvision.models import alexnet
+from torch import autograd
+model = alexnet()
+criterion = CrossEntropyLoss(reduction="none")
+x = torch.randn([16,3,227,227])
+print(x.tanh(dim=1))
